69997e02b4a1192bf32c5ec27ac2baa81aad7585
PreskoIsTheGreatest <preskosemov@abv.bg>
2014-11-08 10:00:33 +0200

    Add spawn spawners script

--- RampartProject/Assets/Scripts/SpawnScript.cs
+++ RampartProject/Assets/Scripts/SpawnScript.cs
@@ -4,24 +4,34 @@ using System.Collections.Generic;
 
 public class SpawnScript : MonoBehaviour {
 
-    public float spawnDelay;
-    public Queue<GameObject> wave;
-    private float lastSpawnTime;
+	public float spawnDelay;
+	public Queue<GameObject> wave;
+	private float lastSpawnTime = 0f;
+	private GameObject lastSpawnedObject;
 	// Use this for initialization
 	void Start () {
 	
 	}
 
 	void FixedUpdate () {
-        if (lastSpawnTime + spawnDelay < Time.time)
-        {
-            Spawn();
-            lastSpawnTime = Time.time;
-        }
+		if (lastSpawnTime + spawnDelay < Time.time && wave.Count>0) 
+		{
+			Spawn();
+			lastSpawnTime = Time.time;
+		}
 	}
 
-    private GameObject Spawn()
-    {
-        return (GameObject)Instantiate(wave.Dequeue(), this.transform.position, this.transform.rotation);
-    }
+	private GameObject Spawn()
+	{
+		if(lastSpawnedObject!=null 
+			&& !lastSpawnedObject.transform.position.Equals(this.transform.position))
+		{
+				lastSpawnedObject = (GameObject)Instantiate(wave.Dequeue(), this.transform.position, this.transform.rotation);
+		}
+		else if (lastSpawnedObject == null)
+		{
+			lastSpawnedObject = (GameObject)Instantiate(wave.Dequeue(), this.transform.position, this.transform.rotation);
+		}
+		return lastSpawnedObject;
+	}
 }
---
+++ RampartProject/Assets/Scripts/SpawnersCreator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnersCreator : MonoBehaviour
+{
+
+    public int countRabitsPerWave;
+    public int countCavemenPerWave ;
+    public int countHarpiesPerWave ;
+    public int countReptilesPerWave ;
+    private Queue<GameObject> enemiesInWave;
+    public string CAVEMAN_NAME = "cavemen";
+    public string RABBIT_NAME = "rabbit";
+    public string HARPY_NAME = "Harpy";
+    public string REPTILE_NAME = "reptile";
+    private const string ENEMIES_FOLDER = "Enemies/";
+    public ushort spawnersCount = 3;
+    public Vector3[] spawnersPosition;
+    public GameObject spawnerPrefab;
+    // Use this for initialization
+    void Start()
+    {
+        if (spawnersPosition.Length == spawnersCount)
+        {
+            enemiesInWave = CreateWave();
+            for (uint i = 0; i < spawnersCount; i++)
+            {
+                GameObject spawner = (GameObject)Instantiate(spawnerPrefab, spawnersPosition[(int)i], Quaternion.identity);
+                SpawnScript spawnerScript = spawner.GetComponent<SpawnScript>();
+                spawnerScript.wave = enemiesInWave;
+                spawnerScript.spawnDelay = 1f;
+            }
+        }
+    }
+
+    private Queue<GameObject> CreateWave()
+    {
+        Queue<GameObject> enemies = new Queue<GameObject>();
+        if (countCavemenPerWave > 0)
+        {
+            AddItemsToQueueByName(enemies, CAVEMAN_NAME, countCavemenPerWave);
+        }
+        if (countHarpiesPerWave > 0)
+        {
+            AddItemsToQueueByName(enemies, HARPY_NAME, countHarpiesPerWave);
+        }
+        if (countRabitsPerWave > 0)
+        {
+            AddItemsToQueueByName(enemies, RABBIT_NAME, countRabitsPerWave);
+        }
+        if (countReptilesPerWave > 0)
+        {
+            AddItemsToQueueByName(enemies, REPTILE_NAME, countReptilesPerWave);
+        }
+        Debug.Log(enemies.Count);
+        return enemies;
+    }
+
+    private void AddItemsToQueueByName(Queue<GameObject> enemies, string enemyName, int count)
+    {
+        GameObject enemy = Resources.Load<GameObject>( ENEMIES_FOLDER+ enemyName) as GameObject;
+        for (int i = 0; i < count; i++)
+        {
+            enemies.Enqueue(enemy);
+        }
+    }
+}
