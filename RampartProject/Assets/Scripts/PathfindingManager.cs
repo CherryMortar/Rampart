114e729e6b5a59320c5b4fc94deb8d363c7b2b22
kalodsun <kalodsun@gmail.com>
2014-11-08 12:40:31 +0200

    Added support for multiple graph nodes per tile

--- RampartProject/Assets/Scripts/PathfindingManager.cs
+++ RampartProject/Assets/Scripts/PathfindingManager.cs
@@ -9,15 +9,11 @@ public class PathfindingManager : MonoBehaviour
     protected List<Seeker> seekers;
     protected PlayFieldSpawner playFieldSpawner;
 
-	void Start () 
-    {
-        
-	}
-	
-	void Update () 
-    {
-	
-	}
+    public int nodesPerTile = 1;
+
+    void Start() { }
+
+    void Update() { }
 
     public void Initialize()
     {
@@ -33,9 +29,9 @@ public class PathfindingManager : MonoBehaviour
         Vector2 tileSize = playFieldSpawner.tileSize;
 
         gridGraph = astarData.AddGraph(typeof(GridGraph)) as GridGraph;
-        gridGraph.width = (int)fieldSize.x;
-        gridGraph.depth = (int)fieldSize.y;
-        gridGraph.nodeSize = (int)playFieldSpawner.tileSize.x;
+        gridGraph.width = (int)fieldSize.x*nodesPerTile;
+        gridGraph.depth = (int)fieldSize.y*nodesPerTile;
+        gridGraph.nodeSize = playFieldSpawner.tileSize.x / nodesPerTile;
         gridGraph.collision.collisionCheck = false;
         gridGraph.collision.heightCheck = false;
         gridGraph.center = playFieldSpawner.GetTileCenter(new Vector2(fieldSize.x / 2, fieldSize.y / 2)) - new Vector3(tileSize.x/2, 0, tileSize.y/2);
@@ -44,19 +40,38 @@ public class PathfindingManager : MonoBehaviour
         AstarPath.active.Scan();
     }
 
-    public void SetWalkable(int index, bool walkable)
+    private List<int> GetNodeIndexesInTile(int tileIndex)
     {
-        gridGraph.nodes[index].Walkable = walkable;
+        List<int> nodes = new List<int>();
+
+        Vector2 fieldSize = playFieldSpawner.fieldSize;
+        int prevLines = (int)(fieldSize.x * nodesPerTile * (tileIndex / (int)fieldSize.x) * nodesPerTile);
+
+        for (int line = 0; line < nodesPerTile; line++)
+        {
+            for (int i = 0; i < nodesPerTile; i++)
+            {
+                nodes.Add((int)(prevLines + line * fieldSize.x * nodesPerTile + (tileIndex % (int)fieldSize.x) * nodesPerTile + i));
+            }
+        }
+
+        return nodes;
+    }
+
+    public void SetWalkable(int tileIndex, bool walkable)
+    {
+        foreach (int i in GetNodeIndexesInTile(tileIndex))
+            gridGraph.nodes[i].Walkable = walkable;
     }
 
     public void SetWalkable(Vector2 tile, bool walkable)
     {
-        gridGraph.nodes[(int)(tile.x + tile.y * playFieldSpawner.fieldSize.x)].Walkable = walkable;
+        SetWalkable((int)(tile.x + tile.y * playFieldSpawner.fieldSize.x), walkable);
     }
 
     public bool IsWalakble(Vector2 tile)
     {
-        return gridGraph.nodes[(int)(tile.x + tile.y * playFieldSpawner.fieldSize.x)].Walkable;
+        return gridGraph.nodes[GetNodeIndexesInTile((int)(tile.x + tile.y * playFieldSpawner.fieldSize.x))[0]].Walkable;
     }
 
     public void FindPath(Vector3 start, Vector3 end, OnPathDelegate callback)
