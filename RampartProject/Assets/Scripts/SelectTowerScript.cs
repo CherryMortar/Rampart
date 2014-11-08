bceff0b92c17d7d2cea9323f30c6eb351dc84d87
EmilianStankov <eminstine@abv.bg>
2014-11-08 18:40:23 +0200

    Remove unimplemented methods

--- RampartProject/Assets/Scripts/SelectTowerScript.cs
+++ RampartProject/Assets/Scripts/SelectTowerScript.cs
@@ -12,24 +12,4 @@ public class SelectTowerScript : MonoBehaviour {
 	{
 		
 	}
-	
-	private GUI generateBox()
-	{
-			
-	}
-	
-	private GUIStyle style() 
-	{
-		GUIStyle style = new GUIStyle();
-		style.fontSize = fontSize;
-		style.normal.textColor = Color.white;
-		style.hover.textColor = Color.white;
-		style.active.textColor = Color.white;
-		style.normal.background = logo;
-		style.hover.background = logo;
-		style.active.background = logo;
-		style.alignment = TextAnchor.MiddleCenter;
-		style.padding.top = paddingTop;
-		return style;
-	}
 }
