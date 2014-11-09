e77aa3cecdce7072742caefaa9df507286976a44
EmilianStankov <eminstine@abv.bg>
2014-11-09 13:59:21 +0200

    Add tower colliders

--- RampartProject/Assets/Scripts/SplashScreenScript.cs
+++ RampartProject/Assets/Scripts/SplashScreenScript.cs
@@ -8,7 +8,7 @@ public class SplashScreenScript : MonoBehaviour {
 	public int fontSize;
 	public int paddingTop;
 	public MainScript mainScript;
-	public MovieTexture video;
+	//public MovieTexture video;
 
 	// Use this for initialization
 	void Start ()
@@ -20,7 +20,7 @@ public class SplashScreenScript : MonoBehaviour {
 	void OnGUI()
 	{
 		GUI.skin.font = font;
-		GUI.DrawTexture (new Rect(0, 0, Screen.width, Screen.height), video);
+		//GUI.DrawTexture (new Rect(0, 0, Screen.width, Screen.height), video);
 		
 		if(GUI.Button(new Rect(0, 0, Screen.width, Screen.height), "Click to start", style()))
 		{
